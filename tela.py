85f98b7df5485dc6d4e1a2b852756150cc90f621
ViniScooper <vviniciuslourenco@gmail.com>
2024-04-23 08:16:59 -0300

    update

--- tela.py
+++ tela.py
@@ -17,42 +17,32 @@ janela.iconbitmap("logo.ico")
 # Define a cor de fundo da janela
 janela.configure(bg="orange")  # Cor de fundo laranja
 
-
+# Adiciona a imagem
+imagem = tk.PhotoImage(file="imgtela.jpg")
+label_imagem = tk.Label(janela, image=imagem, bg="orange")
+label_imagem.pack(pady=10)  # Espaço após a imagem
 
 # Cria uma variável para guardar a escolha do usuário
 escolha = tk.StringVar()
 escolha.set("Selecione uma moeda")
 
-
-
 # Rótulo para instrução
 instrucao_label = tk.Label(janela, text="Escolha uma moeda:", font=('Arial', 14, 'bold'), bg="orange")
-instrucao_label.pack(pady=10)
-
-
+instrucao_label.pack()
 
 # Opções de escolha
 opcoes = ["Dólar", "Euro", "Bitcoin"]
 opcoes_menu = tk.OptionMenu(janela, escolha, *opcoes)
-#opcoes_menu.config(font=('Arial', 12))
 opcoes_menu.config(font=('Arial', 12), bg="PaleGoldenrod", fg="black")  # Cor de fundo branca e texto preto
 opcoes_menu.pack(pady=5)
 
-
-
-
 # Botão para mostrar cotações
-mostrar_button = tk.Button(janela, text="Mostrar Cotação", command=atualizar_cotacoes, font=('NEGRITO', 12, 'bold'), bg="#EEE8AA", fg="black")
+mostrar_button = tk.Button(janela, text="Mostrar Cotação", command=atualizar_cotacoes, font=('Arial', 12, 'bold'), bg="#EEE8AA", fg="black")
 mostrar_button.pack(pady=20)
-
-
 
 # Rótulos para exibir as cotações
 nome_label = tk.Label(janela, text="Moeda: ", font=('Arial', 12), bg="Peru")
 nome_label.pack()
-
-
-
 
 valor_label = tk.Label(janela, text="Valor: ", font=('Arial', 12), bg="PaleGoldenrod")
 valor_label.pack()
